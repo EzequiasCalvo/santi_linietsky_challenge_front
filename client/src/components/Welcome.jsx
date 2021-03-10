9c565762623d952f74f9e6f65091ad12086819c9
ezequias calvo ladmann <calvo.ezequias@gmail.com>
2021-03-10 08:52:56 -0300

    add button to log out

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { TopRightButton, SignUpWrapper } from "../style/login";
+import history from "../history";
 
 const Welcome = () => {
-  return <h1>WELCOME TO LINKED!! 🔥🔥</h1>;
+  const handleClick = () => {
+    localStorage.setItem("isLogged", false);
+    history.push("/");
+  };
+  return (
+    <div>
+      <SignUpWrapper>
+        <h1 style={{ marginRight: "250px" }}>WELCOME TO LINKED!! 🔥🔥</h1>
+        <TopRightButton
+          onClick={handleClick}
+          style={{ backgroundColor: "rgb(234, 72, 72)" }}
+        >
+          Log Out
+        </TopRightButton>
+      </SignUpWrapper>
+    </div>
+  );
 };
 
 export default Welcome;
