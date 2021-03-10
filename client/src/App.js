f0b3302566cfe3dcba36b5219ba9a11522e1e928
ezequias calvo ladmann <calvo.ezequias@gmail.com>
2021-03-10 08:18:15 -0300

    add the welcome component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import history from "./history";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import { MainWrapper } from "./style/login";
+import Welcome from "./components/Welcome";
 
 function App() {
   // useEffect(() => {
@@ -11,13 +11,12 @@ function App() {
   return (
     <div>
       <Router history={history}>
-        <MainWrapper>
-          <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/signup" exact component={SignUp} />
-            <Route path="*" component={() => "404 NOT FOUND"} />
-          </Switch>
-        </MainWrapper>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/signup" exact component={SignUp} />
+          <Route path="/welcome" exact component={Welcome} />
+          <Route path="*" component={() => "404 NOT FOUND"} />
+        </Switch>
       </Router>
       <Login />
     </div>
---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Welcome = () => {
+  return <h1>WELCOME TO LINKED!! 🔥🔥</h1>;
+};
+
+export default Welcome;
